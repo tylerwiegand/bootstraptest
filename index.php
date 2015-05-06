adaf9e44c02db18580be24bdf88392607e4c6dd4
Tyler Wiegand <tyler.wiegand@me.com>
2015-05-06 11:13:06 -0600

    began stuff

---
+++ index.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="utf-8"/>
+		<meta http-equiv="X-UA-Compatible" content="IE=edge"/>
+		<meta name="viewport" content="width=device-width, initial-scale=1"/>
+
+		<!-- Bootstrap Latest compiled and minified CSS -->
+		<link type="text/css" href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.2/css/bootstrap.min.css" rel="stylesheet"/>
+
+		<!-- Optional Bootstrap theme -->
+		<link type="text/css" href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.2/css/bootstrap-theme.min.css" rel="stylesheet"/>
+
+		<!-- ////////////////////////////////////////////////
+		 //// LINK TO YOUR CUSTOM CSS FILES HERE
+		 //// <link type="text/css" href="css/style.css" rel="stylesheet"/>
+		 ///////////////////////////////////////////////////// -->
+
+		<!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+		<!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+		<!--[if lt IE 9]>
+		<script type="text/javascript" src="//oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+		<script type="text/javascript" src="//oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+		<![endif]-->
+
+		<!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+		<script type="text/javascript" src="//ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
+		<script type="text/javascript" src="//cdnjs.cloudflare.com/ajax/libs/jquery.form/3.51/jquery.form.min.js"></script>
+		<script type="text/javascript" src="//ajax.aspnetcdn.com/ajax/jquery.validate/1.12.0/jquery.validate.min.js"></script>
+		<script type="text/javascript" src="//ajax.aspnetcdn.com/ajax/jquery.validate/1.12.0/additional-methods.min.js"></script>
+
+		<!-- Latest compiled and minified Bootstrap JavaScript, all compiled plugins included -->
+		<script type="text/javascript" src="//maxcdn.bootstrapcdn.com/bootstrap/3.3.2/js/bootstrap.min.js"></script>
+		<title>Bootstrap 101 Template (DDC Version!)</title>
+	</head>
+	<body>
+		<div class="container">
+			<h1>About Wizard</h1>
+			<p id="para1">
+			Er alluviale hellingen wegwerpen nu er schippers. Mislukking wat ook voorloopig verdedigen
+			per inlandsche dan. Middelpunt al op in belangrijk specerijen nu. Markt zeker en sinds zijde
+			er. Tandrad koelies na voordat op te. Procede bewogen het slechts brengen are energie. Opening
+			trekken af gevolgd in ze na eronder plaatse.
+			</p>
+			<p id="para2">
+			Rug gelukken des ten gehouden zandlaag middelen bedreven. Dag veld wel daad vast aard wiel vrij.
+			Dank bouw ze doel ze eind de of vele. Al kuil daar na nu toch werk. Onderling resideert beteekentin
+			ad eigenaars plaatsing nu bevolking. Openen na koffie ze lijnen de in. Dient ver ter geeft ellenleven
+			wordt hun later.</p>
+		</div>
+	</body>
+</html>
