43443940e8b4a4d24a0b7227bad21a82c8a7e8aa
Tyler Wiegand <tyler.wiegand@me.com>
2015-05-11 14:03:11 -0600

    changed everything!

--- index.php
+++ index.php
@@ -1,55 +1,20 @@
 <html lang="en">
 	<head>
 		<meta charset="UTF-8" />
-		<meta name="viewport" content="width=device-width, initial-scale=1" />
-		<link type="text/css" href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.2/css/bootstrap.min.css"       rel="stylesheet" />
-		<link type="text/css" href="//maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css" rel="stylesheet">
-		<link type="text/css" href="/lib/bootcamp-coders.css"                                              rel="stylesheet" />
-		<link type="image/x-icon" href="/images/favicon.ico"                                               rel="icon" />
-		<script type="text/javascript" src="//ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
-		<script type="text/javascript" src="//cdnjs.cloudflare.com/ajax/libs/jquery.form/3.51/jquery.form.min.js"></script>
-		<script type="text/javascript" src="//ajax.aspnetcdn.com/ajax/jquery.validate/1.12.0/jquery.validate.min.js"></script>
-		<script type="text/javascript" src="//ajax.aspnetcdn.com/ajax/jquery.validate/1.12.0/additional-methods.min.js"></script>
-		<script type="text/javascript" src="//maxcdn.bootstrapcdn.com/bootstrap/3.3.2/js/bootstrap.min.js"></script>
-		<!-- Google Fonts -->
-		<link href='//fonts.googleapis.com/css?family=Roboto:400,100,300' rel='stylesheet' type='text/css'/>
-
-		<!-- Custom Stylesheet -->
 		<link rel="stylesheet" type="text/css" href="css/bootstrap.css" />
-
-
-		<title>Fancy Bootstrap Sample Form</title>
+		<link rel="javascript" type="text/js" href="js/bootstrap.js" />
+		<title>Starship Wizard</title>
 	</head>
 	<body>
-		<header>
-		</header>
-			<div class="container">
+			<div class="container-fluid">
 			<div class="row">
-				<h1>About Wizard</h1>
-				<div class="row">
-					<div class="col-md-3"><img src="img/wizard1.jpg" alt="Majestic Wizard" class="img-responsive" /></div>
-					<div class="col-md-9">
-						<p id="para1">
-						Er alluviale hellingen wegwerpen nu er schippers. Mislukking wat ook voorloopig verdedigen
-						per inlandsche dan. Middelpunt al op in belangrijk specerijen nu. Markt zeker en sinds zijde
-						er. Tandrad koelies na voordat op te. Procede bewogen het slechts brengen are energie. Opening
-						trekken af gevolgd in ze na eronder plaatse.
-						</p>
-					</div>
-				</div>
-			<div class="row">
-				<div class="col-md-12">
-					<p id="para2">
-					Rug gelukken des ten gehouden zandlaag middelen bedreven. Dag veld wel daad vast aard wiel vrij.
-					Dank bouw ze doel ze eind de of vele. Al kuil daar na nu toch werk. Onderling resideert beteekentin
-					ad eigenaars plaatsing nu bevolking. Openen na koffie ze lijnen de in. Dient ver ter geeft ellenleven
-					wordt hun later.</p>
-				</div>
-			</div>
-		<div class="row">
-			<div class="col-md-4"><img src="img/eviltroll.jpg" alt="evil troll" class="img-responsive" /></div>
-			<div class="col-md-4"><img src="img/wizardhat.jpg" alt="wizard hat" class="img-responsive" ></div>
-			<div class="col-md-4"><img src="img/wizardsword.png" alt="wizard sword" class="img-responsive" ></div>
-		</div>
+				<h1>About Steareihusahip</h1>
+				<p class="layered-paper" id="text1">
+					These are the voyages of the Starship Enterprise. Its continuing mission, to explore strange new worlds, to seek out new life and new civilizations, to boldly go where no one has gone before. We need to neutralize the homing signal. Each unit has total environmental control, gravity, temperature, atmosphere, light, in a protective field. Sensors show energy readings in your area. We had a forced chamber explosion in the resonator coil. Field strength has increased by 3,000 percent.
+				</p>
+
+				<p class="layered-paper" id="text2">
+					Sensors indicate no shuttle or other ships in this sector. According to coordinates, we have travelled 7,000 light years and are located near the system J-25. Tractor beam released, sir. Force field maintaining our hull integrity. Damage report? Sections 27, 28 and 29 on decks four, five and six destroyed. Without our shields, at this range it is probable a photon detonation could destroy the Enterprise.
+				</p>
 	</body>
 </html>
