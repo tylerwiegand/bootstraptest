07de76a92a2788507fa9fa746805de20f9d612a3
Tyler Wiegand <tyler.wiegand@me.com>
2015-05-11 14:05:35 -0600

    changed everything!

--- index.php
+++ index.php
@@ -9,11 +9,11 @@
 			<div class="container-fluid">
 			<div class="row">
 				<h1>About Steareihusahip</h1>
-				<p class="layered-paper" id="text1">
-					These are the voyages of the Starship Enterprise. Its continuing mission, to explore strange new worlds, to seek out new life and new civilizations, to boldly go where no one has gone before. We need to neutralize the homing signal. Each unit has total environmental control, gravity, temperature, atmosphere, light, in a protective field. Sensors show energy readings in your area. We had a forced chamber explosion in the resonator coil. Field strength has increased by 3,000 percent.
+				<p class="layered-paper" id="text1" onclick="clickEvent1();">
+				These are the voyages of the Starship Enterprise. Its continuing mission, to explore strange new worlds, to seek out new life and new civilizations, to boldly go where no one has gone before. We need to neutralize the homing signal. Each unit has total environmental control, gravity, temperature, atmosphere, light, in a protective field. Sensors show energy readings in your area. We had a forced chamber explosion in the resonator coil. Field strength has increased by 3,000 percent.
 				</p>
 
-				<p class="layered-paper" id="text2">
+				<p class="layered-paper" id="text2" onclick="clickEvent2();">
 					Sensors indicate no shuttle or other ships in this sector. According to coordinates, we have travelled 7,000 light years and are located near the system J-25. Tractor beam released, sir. Force field maintaining our hull integrity. Damage report? Sections 27, 28 and 29 on decks four, five and six destroyed. Without our shields, at this range it is probable a photon detonation could destroy the Enterprise.
 				</p>
 	</body>
